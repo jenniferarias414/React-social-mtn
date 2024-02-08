ea2c27409e3ff29fe9087e67cde8a0771a6a1008
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-02-07 18:26:45 -0600

    part 2 complete

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -1,11 +1,25 @@
+// const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
+// require('dotenv').config();
+import {config} from 'dotenv';
+config();
+const SECRET = process.env.SECRET
+
 export const login = async (req, res) => {
+     const { username, password } = req.body;
+     const token = createToken(username, password);
+
     console.log('login')
-    res.sendStatus(200)
+    res.status(200).send(token)
 }
 
 export const register = async (req, res) => {
     console.log('register')
     res.sendStatus(200)
+}
+
+const createToken = (username, id) => {
+    return jwt.sign({username, id}, SECRET, {expiresIn: '2 days'})
 }
 
 // module.exports = {
--- src/server/main.js
+++ src/server/main.js
@@ -2,6 +2,7 @@ import express from "express";
 import ViteExpress from "vite-express";
 import {addPost, getAllPosts, getCurrentUserPosts, editPost, deletePost} from './controllers/posts.js';
 import {register, login} from './controllers/auth.js';
+import { isAuthenticated } from "./middleware/isAuthenticated.js";
 
 const app = express();
 app.use(express.json());
@@ -12,9 +13,9 @@ app.post('/login', login);
 app.get('/posts', getAllPosts);
 
 app.get('/userposts/:userId', getCurrentUserPosts);
-app.post('/posts', addPost);
-app.put('/posts/:id', editPost);
-app.delete('/posts/:id', deletePost);
+app.post('/posts', isAuthenticated, addPost);
+app.put('/posts/:id', isAuthenticated, editPost);
+app.delete('/posts/:id', isAuthenticated, deletePost);
 
 // app.get("/hello", (req, res) => {
 //   res.send("Hello Vite + React!");
