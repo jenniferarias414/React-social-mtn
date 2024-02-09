beaf3144cf38e6528bb3b15bc57df0df3c6f5cde
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-02-09 05:51:37 -0600

    login with bcrypt

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -4,21 +4,74 @@ import jwt from 'jsonwebtoken';
 import {config} from 'dotenv';
 config();
 import {User} from '../models/user.js';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 const SECRET = process.env.SECRET
 
 export const login = async (req, res) => {
-     const { username, password } = req.body;
-     const token = createToken(username, password);
+  try {
+    const { username, password } = req.body;
+    const foundUser = await User.findOne({ where: { username: username } }); //why not also pw:pw?
 
-    console.log('login')
-    res.status(200).send(token)
+    if (foundUser) {
+      const isAuthenticated = bcrypt.compareSync(
+        password,
+        foundUser.hashedPass
+      );
+      if (isAuthenticated) {
+        const token = createToken(
+          foundUser.dataValues.username,
+          foundUser.dataValues.id
+        );
+        // Set expiration time for token (two days)
+        const exp = Date.now() + 1000 * 60 * 60 * 48;
+
+        res.status(200).send({
+          username: foundUser.dataValues.username,
+          userId: foundUser.dataValues.id,
+          token: token,
+          exp: exp,
+        });
+      } else {
+        return res.status(400).send({ message: "Password is incorrect." });
+      }
+    } else {
+      return res.status(400).send({ message: "User does not exist." });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(400).send(err);
+  }
 }
 
 export const register = async (req, res) => {
-    console.log('register')
-    res.sendStatus(200)
+    try {
+        const {username, password} = req.body;
+        const foundUser = await User.findOne({ where: { username: username } });
+
+        if (foundUser) {
+            return res.status(400).send({message: 'user with this username already exists'})
+        } else {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+
+            const newUser = await User.create({username: username, hashedPass: hash});
+
+            const token = createToken(newUser.dataValues.username, newUser.dataValues.id);
+            // Set expiration time for token (two days)
+            const exp = Date.now() + 1000 * 60 * 60 * 48;
+
+            res.status(200).send({
+                username: newUser.dataValues.username,
+                userId: newUser.dataValues.id,
+                token: token,
+                exp: exp
+            });
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(400).send(err)
+    }
 }
 
 const createToken = (username, id) => {
